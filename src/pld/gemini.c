90b655f33563399bf422a3f0f5aa62a94a5b88de
Kok, Chern Yee <chernyee.kok@rapidsilicon.com>
2023-11-27 16:45:18 +0800

    fix pld command change issue with latest openocd updates
    Signed-off-by: Kok, Chern Yee <chernyee.kok@rapidsilicon.com>

--- src/pld/gemini.c
+++ src/pld/gemini.c
@@ -892,21 +892,21 @@ PLD_CREATE_COMMAND_HANDLER(gemini_pld_create_command)
 	struct gemini_pld_device_t *gemini_info;
 	int ret = ERROR_OK;
 
-	if (CMD_ARGC < 2)
+	if (CMD_ARGC < 3)
 		return ERROR_COMMAND_SYNTAX_ERROR;
 
 	// allocate memory for devices
 	gemini_info = malloc(sizeof(struct gemini_pld_device_t));
-	gemini_info->count = CMD_ARGC - 1;
+	gemini_info->count = CMD_ARGC - 2;
 	gemini_info->target_info = malloc(sizeof(struct target_info_t) * gemini_info->count);
 
 	// enumerate all passed in targets
 	for (uint32_t i = 0; i < gemini_info->count; ++i)
 	{
-		struct target *target = get_target(CMD_ARGV[i+1]);
+		struct target *target = get_target(CMD_ARGV[i+2]);
 		if (!target || !target->tap)
 		{
-			command_print(CMD, "Target: %s is invalid", CMD_ARGV[i+1]);
+			command_print(CMD, "Target: %s is invalid", CMD_ARGV[i+2]);
 			ret = ERROR_FAIL;
 			break;
 		}
